d5dc8369a0b4ce3f15d3136ec1efbcfffe2c9580
Groodoo <graemecubitt@gmail.com>
2026-04-08 13:11:40 -0700

    Updated auth

--- app/api/entries/route.ts
+++ app/api/entries/route.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 // Create the supabase connection
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
 // POST — save a new entry
